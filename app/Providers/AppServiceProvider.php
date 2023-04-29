ed0182a776d54e2e141986f13a27fe3e56fc3bbf
Prem <aniiyaprem123@gmail.com>
2023-04-29 19:31:17 +0530

    New updated code

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -9,17 +9,21 @@ class AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
+     *
+     * @return void
      */
-    public function register(): void
+    public function register()
     {
         //
     }
 
     /**
      * Bootstrap any application services.
+     *
+     * @return void
      */
-    public function boot(): void
+    public function boot()
     {
-        Paginator::useBootstrapFive();
+        Paginator::useBootstrap();
     }
 }
